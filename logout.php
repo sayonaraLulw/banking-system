7fd149888cdbd0f945723f0d3fbb7a9d6bfedfb6
Felizian <>
2023-03-16 10:14:20 +0100

    delet files add README

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-// TODO - Session starten
-session_start();
-// TODO - Session leeren
-$_SESSION = array();
-session_destroy();
-// TODO - Weiterleiten auf login.php
-header('Location: login.php');
-?>
